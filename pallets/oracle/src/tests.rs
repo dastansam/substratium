3306d7e6fa5346bfd53dcb15fcb93522fbf314a9
dastansam <dastan.samatov.33@gmail.com>
2022-11-01 02:59:38 +0300

    Test fix

--- pallets/oracle/src/tests.rs
+++ pallets/oracle/src/tests.rs
@@ -69,23 +69,23 @@ fn test_max_events_age() {
 		}
 
 		// Make sure the feed has 100 events
-		let feed_event = OracleModule::feed();
+		let feed = OracleModule::feed();
 
-		assert_eq!(feed_event.len(), 100);
+		assert_eq!(feed.len(), 100);
 
 		// Increment block number by 1 hour and 51 blocks, to see if events are removed
 		run_to_block(51 + HOURS as u64);
 
 		// Check if the first event was discarded
-		let new_feed_event = OracleModule::feed();
+		let new_feed = OracleModule::feed();
 
 		// We have 50 events in the feed, even though we increment by 51 blocks
 		// This is because events are filtered out when block is finalized
-		assert_eq!(new_feed_event.len(), 50);
+		assert_eq!(new_feed.len(), 50);
 
 		// Check the order of removed events, they should be in the order of oldest to newest
 		for i in 0..50 {
-			assert_eq!(new_feed_event[i], FeedEventOf::<Test> {
+			assert_eq!(new_feed[i], FeedEventOf::<Test> {
 				value: vec_to_bounded_vec::<Test>(format!("Dummy value for testing - {}", i + 50).as_bytes().to_vec()),
 				block_number: (i + 50) as u64,
 			});
